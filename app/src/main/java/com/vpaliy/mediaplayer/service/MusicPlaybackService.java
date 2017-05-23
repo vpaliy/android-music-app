ce6d47d209a4ea5322b2165f7c0b91a2067f0f77
vpaliy <vpaliy97@gmail.com>
2017-05-22 21:00:06 -0400

    add media service

---
+++ app/src/main/java/com/vpaliy/mediaplayer/service/MusicPlaybackService.java
@@ -0,0 +1,61 @@
+package com.vpaliy.mediaplayer.service;
+
+import android.os.Bundle;
+import android.support.v4.media.MediaBrowserCompat;
+import android.support.v4.media.MediaBrowserServiceCompat;
+import java.util.List;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.media.session.MediaSessionCompat;
+import android.support.v4.media.session.PlaybackStateCompat;
+
+
+public class MusicPlaybackService extends MediaBrowserServiceCompat {
+
+    private static final String LOG_TAG=MusicPlaybackService.class.getSimpleName();
+    public static final String MEDIA_ID_ROOT="root";
+    public static final String MEDIA_ID_EMPTY_ROOT="empty_root";
+
+    private MediaSessionCompat mediaSession;
+    private PlaybackStateCompat.Builder stateBuilder;
+
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        // Create a MediaSessionCompat
+        mediaSession = new MediaSessionCompat(getApplicationContext(), LOG_TAG);
+
+        // Enable callbacks from MediaButtons and TransportControls
+        mediaSession.setFlags(
+                MediaSessionCompat.FLAG_HANDLES_MEDIA_BUTTONS |
+                        MediaSessionCompat.FLAG_HANDLES_TRANSPORT_CONTROLS);
+
+        // Set an initial PlaybackState with ACTION_PLAY, so media buttons can start the player
+        stateBuilder = new PlaybackStateCompat.Builder()
+                .setActions(
+                        PlaybackStateCompat.ACTION_PLAY |
+                                PlaybackStateCompat.ACTION_PLAY_PAUSE);
+        mediaSession.setPlaybackState(stateBuilder.build());
+
+
+        // Set the session's token so that client activities can communicate with it.
+        setSessionToken(mediaSession.getSessionToken());
+
+    }
+
+    @Nullable
+    @Override
+    public BrowserRoot onGetRoot(@NonNull String clientPackageName, int clientUid, @Nullable Bundle rootHints) {
+        if(!clientPackageName.equals(getPackageName())){
+            return new BrowserRoot(MEDIA_ID_ROOT,null);
+        }
+        return new BrowserRoot(MEDIA_ID_EMPTY_ROOT,null);
+    }
+
+    @Override
+    public void onLoadChildren(@NonNull String parentId, @NonNull Result<List<MediaBrowserCompat.MediaItem>> result) {
+
+    }
+}
