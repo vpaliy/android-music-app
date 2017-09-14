60e16b34b3366674718e33745cc2693a8fddc191
vpaliy <vpaliy97@gmail.com>
2017-09-14 12:40:16 -0400

    data layer :: configure remote fetching

--- app/src/main/java/com/vpaliy/mediaplayer/data/MusicRepository.kt
+++ app/src/main/java/com/vpaliy/mediaplayer/data/MusicRepository.kt
@@ -1,13 +1,22 @@
 package com.vpaliy.mediaplayer.data
 
+import com.vpaliy.mediaplayer.data.mapper.Mapper
 import com.vpaliy.mediaplayer.domain.Repository
 import com.vpaliy.mediaplayer.domain.model.Track
+import com.vpaliy.soundcloud.SoundCloudService
+import com.vpaliy.soundcloud.model.Page
+import com.vpaliy.soundcloud.model.TrackEntity
 import io.reactivex.Completable
 import io.reactivex.Single
+import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
-class MusicRepository:Repository{
+class MusicRepository @Inject constructor(val mapper: Mapper<Track,TrackEntity>,
+                                          val service:SoundCloudService):Repository{
+
+    private var page:Page<TrackEntity>?=null
+
     override fun fetchHistory(): Single<List<Track>> {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
@@ -16,12 +25,31 @@ class MusicRepository:Repository{
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
-    override fun query(query: String?): Single<List<Track>> {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    override fun query(query: String?): Single<List<Track?>> {
+        return service.searchTracksPage(TrackEntity.Filter
+                .start().byName(query)
+                .withPagination()
+                .limit(100)
+                .createOptions())
+                .map({result->
+                    page=result
+                    result.collection
+                }).map(mapper::map)
     }
 
-    override fun nextPage(): Single<List<Track>> {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    override fun nextPage(): Single<List<Track?>> {
+        if(page!=null){
+            return service.searchTracksPage(TrackEntity.Filter
+                    .start().nextPage(page)
+                    .withPagination()
+                    .limit(100)
+                    .createOptions())
+                    .map({result->
+                        page=result
+                        result.collection
+                    }).map(mapper::map)
+        }
+        return Single.error(IllegalArgumentException("No more data"))
     }
 
     override fun like(track: Track?): Completable {
--- app/src/main/java/com/vpaliy/mediaplayer/domain/Repository.kt
+++ app/src/main/java/com/vpaliy/mediaplayer/domain/Repository.kt
@@ -7,7 +7,7 @@ import io.reactivex.Single
 interface Repository {
     fun fetchHistory():Single<List<Track>>
     fun fetchLiked():Single<List<Track>>
-    fun query(query:String?):Single<List<Track>>
-    fun nextPage(): Single<List<Track>>
+    fun query(query:String?):Single<List<Track?>>
+    fun nextPage(): Single<List<Track?>>
     fun like(track:Track?):Completable
 }
